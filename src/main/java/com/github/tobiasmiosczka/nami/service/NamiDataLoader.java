5975bf38791e12c6f59d105a08371eebe2224ad3
TobiasMiosczka <6351397+tobiasmiosczka@users.noreply.github.com>
2022-10-11 03:11:22 +0200

    [NO-TICKET] Refactoring

--- src/main/java/com/github/tobiasmiosczka/nami/service/NamiDataLoader.java
+++ src/main/java/com/github/tobiasmiosczka/nami/service/NamiDataLoader.java
@@ -4,7 +4,6 @@ import nami.connector.NamiConnector;
 import nami.connector.namitypes.NamiMitglied;
 import nami.connector.namitypes.NamiSearchedValues;
 
-import java.util.Collection;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class NamiDataLoader {
@@ -16,24 +15,18 @@ public class NamiDataLoader {
 	}
 
 	public static void load(NamiConnector connector, NamiSearchedValues searchedValues, Listener listener) {
-		Thread thread = new Thread(() -> {
-			try {
-				long startTime = System.currentTimeMillis();
-				Collection<NamiMitglied> result = connector.getAllResults(searchedValues).get();
-				AtomicInteger i = new AtomicInteger();
-				for (NamiMitglied member : result) {
-					connector.getMitgliedById(member.getId())
-							.thenAccept(m -> listener.onUpdate(i.getAndIncrement(), result.size(), m))
-							.exceptionally(e -> {
-								listener.onException("Fehler beim Laden der Mitgliedsdaten.", e);
-								return null; });
-				}
-				listener.onDone((System.currentTimeMillis() - startTime));
-			} catch (Exception e) {
-				listener.onException("Fehler beim Laden der Mitgliedsdaten.", e);
+		long startTime = System.currentTimeMillis();
+		AtomicInteger i = new AtomicInteger();
+		connector.getAllResults(searchedValues).thenAccept(list -> {
+			for (NamiMitglied member : list) {
+				connector.getMitgliedById(member.getId())
+						.thenAccept(m -> listener.onUpdate(i.getAndIncrement(), list.size(), m))
+						.exceptionally(e -> {
+							listener.onException("Fehler beim Laden der Mitgliedsdaten.", e);
+							return null;
+						});
 			}
+			listener.onDone((System.currentTimeMillis() - startTime));
 		});
-		thread.setDaemon(true);
-		thread.start();
 	}
 }
